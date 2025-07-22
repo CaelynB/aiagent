a89f21530e9e8cf68f704a535ec99a64d64ca800
CaelynB <caelynlballiet@gmail.com>
2025-07-22 17:17:17 -0500

    feat: implement Gemini API client and update .gitignore to keep API key private

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from aiagent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    # load the environment variables from the .env file
+    load_dotenv()
+
+    # read the API key
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    # create a new instance of a Gemini client using the API key
+    client = genai.Client(api_key=api_key)
+
+    # get a response from the Gemini model
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+    # print the response and the number of tokens consumed
+    print(f"Response: {response.text}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
